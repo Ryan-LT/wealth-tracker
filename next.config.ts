f3b790ebf3d62cfd9fb20dd4c6c0826a1e037682
Ryan Le <thinh.le.sdev@gmail.com>
2026-05-10 22:04:10 +0700

    Integrate code-inspector-plugin into Next.js configuration and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { codeInspectorPlugin } from "code-inspector-plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      ...codeInspectorPlugin({
+        bundler: "turbopack",
+      }),
+    },
+  },
 };
 
 export default nextConfig;
